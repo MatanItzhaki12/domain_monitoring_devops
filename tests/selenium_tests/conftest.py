70ec458472e49463fdcbdc597c868a4beb2153a4
imse <im.se@outlook.com>
2025-11-20 19:47:13 +0200

    a

--- tests/selenium_tests/conftest.py
+++ tests/selenium_tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import shutil
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -8,7 +9,7 @@ from selenium.webdriver.chrome.service import Service
 def driver():
     chrome_options = Options()
 
-    chrome_options.add_argument("--disable-features=DownloadBubble")  # new in Chrome 110+
+    chrome_options.add_argument("--disable-features=DownloadBubble")
     chrome_options.add_argument("--safebrowsing-disable-download-protection")
     chrome_options.add_argument("--safebrowsing-disable-extension-blacklist")
     chrome_options.add_argument("--disable-component-update")
@@ -18,36 +19,30 @@ def driver():
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--remote-debugging-port=9222")
 
-    # THIS IS THE KEY LINE FOR FILE UPLOADS ON LINUX:
     chrome_options.add_experimental_option("useAutomationExtension", False)
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
-    # Most important for file upload in Linux containers:
     chrome_options.add_argument("--disable-web-security")
-    chrome_options.add_argument("--allow-file-access-from-files")   # sometimes needed
+    chrome_options.add_argument("--allow-file-access-from-files")
+    chrome_options.add_argument("--enable-local-file-accesses")
 
-    # Stable headless mode
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--window-size=1920,1080")
-
-
-    chrome_options.add_argument("--allow-file-access-from-files")
-    chrome_options.add_argument("--disable-web-security")
-    chrome_options.add_argument("--enable-local-file-accesses")
-    chrome_options.add_argument("--disable-site-isolation-trials")
-
-
-    # Safe additional flags
     chrome_options.add_argument("--disable-infobars")
     chrome_options.add_argument("--disable-extensions")
 
-    chrome_options.add_argument("--disable-features=LockProfileForFileUploads,DownloadBubble,DownloadBubbleV2")
+    # -------- FIX: auto-detect chromedriver ----------
+    driver_path = shutil.which("chromedriver")
+    if not driver_path:
+        raise RuntimeError("chromedriver not found in PATH")
 
-    service = Service("/usr/bin/chromedriver")
+    service = Service(driver_path)
+
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     yield driver
     driver.quit()
+
 
 @pytest.fixture
 def base_url():
