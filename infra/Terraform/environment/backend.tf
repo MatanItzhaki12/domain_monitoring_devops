420fea5157bf6c9641133b5f6ade60c28fed7f2a
Assaf <assaf.naaman.work@gmail.com>
2025-12-08 18:11:03 +0200

    ffdf

--- infra/Terraform/environment/backend.tf
+++ infra/Terraform/environment/backend.tf
@@ -3,7 +3,7 @@ terraform {
         bucket         = "group2-terraform-bucket-2025-774411"
         key            = "terraform.tfstate"
         region         = "us-east-2"
-        dynamodb_table = "terraform-state-locking"
+        dynamodb_table = "group2-terraform-state-locking"
         encrypt        = true
     }
 } 
