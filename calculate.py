6d21142e4a879f3884aa99aed708fa07296214ba
Assaf <assaf.naaman.work@gmail.com>
2026-01-15 02:52:48 +0200

    reversed matrix and updated calculate.py

--- calculate.py
+++ calculate.py
@@ -1,10 +1,10 @@
 import json
 import os
-#check if file exists and make it is a file if not create it
+# check if file exists and make it is a file if not create it
 
 if not os.path.exists(os.path.join(os.path.dirname(__file__), 'matrix.json')):
     with open(os.path.join(os.path.dirname(__file__), 'matrix.json'), 'w') as f:
-        matrix = {"frontend": {}}
+        matrix = {"backend": {}}
         json.dump(matrix, f)
 
 with open(os.path.join(os.path.dirname(__file__), 'matrix.json')) as f:
@@ -17,10 +17,10 @@ backend_version = os.environ.get(
     "BACKEND_VERSION", "localhost"
 )
 
-if frontend_version not in matrix["frontend"]:
-    matrix["frontend"][frontend_version] = []
+if backend_version not in matrix["backend"]:
+    matrix["backend"][backend_version] = []
 
-matrix["frontend"][frontend_version].append(backend_version)
+matrix["backend"][backend_version].append(frontend_version)
 
 with open(os.path.join(os.path.dirname(__file__), 'matrix.json'), 'w') as f:
     json.dump(matrix, f, indent=2)
